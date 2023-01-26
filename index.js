409d8354e9236c197af38a4c817c0243282bf374
Piyush khare <113968283+piyush6716721@users.noreply.github.com>
2023-01-27 00:54:12 +0530

    quiz

---
+++ index.js
@@ -0,0 +1,108 @@
+//Question bank
+var questionBank= [
+    {
+        question : 'translate "क्या राम खाना खाता है ?"',
+        option : ['Is ram eating the food?','Does ram eat the food?','What ram eat the food?','Is ram eating the food?'],
+        answer : 'Does ram eat the food?'
+    },
+    {
+        question : '  What is the capital of Tripura?  ',
+        option : ['Agartala', 'Assam','Kohima, ',' Sikkim'],
+        answer : 'Agartala'
+    },
+    {
+        question : ' Correct spelling of  "महेंद्र सिंह धोनी"     ?',
+        option : ['  Mehendre singh Dhoni',' Mahendre singh dhoni ','  Mahendra singh dhoni ','Mehendre sengh dhoni'],
+        answer : '  Mahendra singh dhoni '
+    },
+    {
+        question : 'भारत किस सन में आजाद हुआ था?',
+        option : ['1981 ','1982 ','1947',' 1947'],
+        answer : '1947'
+    },
+    {
+        question : 'भारत की पहली महिला प्रधानमंत्री कौन थी?',
+        option : ['सिरिमावो भंडारनायके','विगडीस फिनबोगडॉटिर','प्रतिभा पाटिल','इंदिरा गांधी'],
+        answer : 'इंदिरा गांधी'
+    }
+    
+    
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
